0672bc200a791b151371017c8ea7a692a56ddd23
Kirti Swaraj <Sai@KirtiSwaraj>
2020-10-04 23:55:20 +0530

    [kirti]Add. Check for the option

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace SnakeAndLadder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Snake and Ladder game for single player\n");
+            int startPosition = 0;
+            const int noPlay = 1;
+            const int ladder = 2;
+            const int snake = 3;
+            int jump = 0;
+            Console.WriteLine("Start Position= " + startPosition);
+
+            //Rolling of die to get a random number
+            Random random = new Random();
+            int diceValue = random.Next(1, 7);
+            Console.WriteLine("Dice value= " + diceValue);
+
+            //Options for the player (No play, Ladder, Snake)
+            int options = random.Next(1, 4);
+            switch (options)
+            {
+                case noPlay:
+                    jump = 0;
+                    Console.WriteLine("No play: Jump= " + jump);
+                    break;
+                case ladder:
+                    jump = diceValue;
+                    Console.WriteLine("Ladder: Jump= " + jump);
+                    Console.WriteLine("New position="+jump);
+                    break;
+                case snake:
+                    jump = -diceValue;
+                    Console.WriteLine("Snake: Jump= " + jump);
+                    if (jump < 0)
+                        jump = 0;
+                    Console.WriteLine("New position=" + jump);
+                    break;
+            }
+
+            Console.ReadKey();
+        }
+
+    }
+}
