e36b92db4284e99e056044a47825f5b79577f589
Kirti Swaraj <Sai@KirtiSwaraj>
2020-10-04 23:49:00 +0530

    [kirti]Add. Roll the dice

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace SnakeAndLadder
+{
+    class Program
+    {
+       static void Main(string[] args)
+            {
+                Console.WriteLine("Snake and Ladder game for single player\n");
+                int startPosition = 0;
+                Console.WriteLine("Start Position= " + startPosition);
+
+                //Rolling of die to get a random number
+                Random random = new Random();
+                int diceValue = random.Next(1, 7);
+                Console.WriteLine("Dice value= " + diceValue);
+            Console.ReadKey();
+            }
+
+        }
+    }
