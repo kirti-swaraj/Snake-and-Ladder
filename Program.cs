9c30070a52e9f2b522deb9b9dd81144bce103f20
Kirti Swaraj <Sai@KirtiSwaraj>
2020-10-04 23:40:36 +0530

    [Kirti] Add. Starting of the game with starting position

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SnakeAndLadder
+{
+     class Program
+        {
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Snake and Ladder game for single player\n");
+                int startPosition = 0;
+                Console.WriteLine("Start Position= " + startPosition);
+                Console.ReadKey();
+            }
+
+        }
+    }
